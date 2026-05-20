cf3e36124cfed8f02f89c5aea1291944ff4125c9
Misha <axxuetam@gmail.com>
2026-05-20 09:55:19 +0500

    16 шаг продолжить

---
+++ ShopContent/Classes/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopContent.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=localhost;database=ShopContent;port=3306;uid=root;pwd=;";
+
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection conn = new SqlConnection(config);
+            conn.Open();
+            return conn;
+        }
+
+        public static SqlDataReader Query(string sql, out SqlConnection conn)
+        {
+            conn = OpenConnection();
+            return new SqlCommand(sql, conn).ExecuteReader();
+        }
+
+        public static SqlConnection CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
---
+++ ShopContent/Modell/Items.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopContent.Modell
+{
+    public class Items : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); } 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop="")
+        {
+            if (PropertyChanged != null)
+                ProperyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
