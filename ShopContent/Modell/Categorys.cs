47a037064efebf7c8940e6f30c7e82c6d1a77558
Misha <axxuetam@gmail.com>
2026-05-21 09:00:56 +0500

    Разработанные модели данных.

---
+++ ShopContent/Modell/Categorys.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopContent.Modell
+{
+    public class Categorys : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set {  name = value; OnPropertyChanged("Name"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                ProperyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
--- ShopContent/Modell/Items.cs
+++ ShopContent/Modell/Items.cs
@@ -17,6 +17,46 @@ namespace ShopContent.Modell
             set { id = value; OnPropertyChanged("Id"); } 
         }
 
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value; OnPropertyChanged("Name");
+            }
+        }
+
+        private double price;
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                price = value; OnPropertyChanged("Price");
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value; OnPropertyChanged("Description");
+            }
+        }
+
+        private Categorys category;
+        public Categorys Category
+        {
+            get { return category; }
+            set
+            {
+                category = value; OnPropertyChanged("Category");
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string prop="")
